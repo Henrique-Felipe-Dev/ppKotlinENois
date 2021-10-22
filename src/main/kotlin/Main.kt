afbe08f4fd2ae2baaadd13b6c1b764b348f9bd50
Henrique Felipe <henrique.silva.felipe459@gmail.com>
2021-10-22 14:58:50 -0300

    commit inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    println("<???> Opa, eae?")
+    println("<Rogério> De boas?")
+    println("<???> Not bad")
+    println("<Rogério> What's your name?")
+    println("<???> My name is Milani")
+    println("<Rogério> Nice to meet you, Milani")
+    println("<Milani> Nice to meet you too")
+
+}
